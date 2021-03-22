1eab8e8fc3197c126fbce0308ab96a77f906c16e
SteveBee123 <startupsteve2@gmail.com>
2021-03-22 19:07:51 -0400

    new script code

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 console.log('\- Welcome to the \"Mixed Messages\" Project'); 
-//Enter Birthday:
-function getSign() {
-    let sign = '';
+//Birthday Variables:
+let bMonth = 0;
+let bDayNum = 0;
 
+//Call 'getSign' function after saving USER-INPUT...
+function getSign(bMonth,bDayNum) {
+    let sign = '';
     const signList = {
         _Cap : 'Capricorn',
         _Aqua : 'Aquarius',
@@ -17,9 +20,21 @@ function getSign() {
         _Scorp : 'Scorpio',
         _Sagg : 'Sagittarius'
     }
+    if (bMonth > 12 || bDayNum > 31) {
+        console.log('I\'m Sorry. Please enter another number.');
+    }
+    else if (bMonth===12 && bDayNum >= 22 || bMonth===1 && ){
+    
+    }
+    else if (bMonth===1 && bDayNum <= 19){
 
-    console.log(signList._Gemin);
-
+    }
+    
+    
+    
+    
+    //console.log('- Your sign is: ' + signList._Gemin);
     return sign;
 }
-getSign();
+getSign(bMonth,bDayNum);
+
