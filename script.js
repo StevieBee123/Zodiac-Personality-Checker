100fc537a9518e53b8a479080e2a4a13f6b4e95c
SteveBee123 <startupsteve2@gmail.com>
2021-03-22 12:54:10 -0400

    add bday function - getSign()

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log('\- Welcome to the \"Mixed Messages\" Project');
+
+console.log('\- Welcome to the \"Mixed Messages\" Project');
+ 
+//Enter Birthday:
+function getSign() {
+    let sign = '';
+
+    const signList = {
+        _Cap : 'Capricorn',
+        _Aqua : 'Aquarius',
+        _Pisc : 'Pisces',
+        _Ari : 'Aries',
+        _Taur : 'Taurus',
+        _Gemin : 'Gemini',
+        _Cancc : 'Cancer',
+        _Leo : 'Leo',
+        _Vgo : 'Virgo',
+        _Lib : 'Libra',
+        _Scorp : 'Scorpio',
+        _Sagg : 'Sagittarius'
+    }
+
+    console.log(signList._Gemin);
+
+    return sign;
+}
+getSign();
