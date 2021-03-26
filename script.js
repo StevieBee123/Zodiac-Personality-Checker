46658af38df033873550d8bb437b1bb6889f0c53
SteveBee123 <startupsteve2@gmail.com>
2021-03-25 23:58:26 -0400

    all mood quotes added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log('\n\- Welcome to the \"Mixed Messages\" Project\n00000');
+console.log('\n\- Welcome to the \"Mixed Messages\" Project\n');
 console.log('Please enter your \'BirthMonth\' followed by the day of the month\: \n');
 //Birthday Variables:
 let bMonth = 0;
@@ -81,15 +81,30 @@ const getMood = (moodChoice)=>{
     }
     else if(mC===m){
         let arrMoodyQuotes = [
-
-
+                'Never go to bed mad\. Stay up and fight\.\”\-Phylis Diller\/Actress',
+                'Anybody can become angry \— that is easy\, but to be angry with the right person and to the right degree and at the right time and for the right purpose\, and in the right way — that is not within everybodys power and is not easy.”-Aristotle/Philosopher', 
+                'Bitterness is like cancer\. It eats upon the host\. But anger is like fire\. It burns it all clean\.\”\-Maya Angelou',
+                'When angry, count four. When very angry, swear\.\”\-Mark Twain\/Author',
+                'Anger, resentment and jealousy doesnt change the heart of others-- it only changes yours.”-Shannon Alder/auth',
+                'How much more grievous are the consequences of anger than the causes of it.”-Marcus Aurelius/Philosopher',
+                'Whoa, who peed in your Cheerios?”-Becca Fitzpatrick/Actress',
+                'There are two things a person should never be angry at, what they can help, and what they cannot.”-Plato ',
+                'Men in rage strike those that wish them best.”-Shakespeare/Author',
+                'Anger makes you smaller, while forgiveness forces you to grow beyond what you are.”-Cherie Carter-Scott'
             ];
     }
     else if(mC===dontCare){
         let arrDontCareQuotes = [
-
-
-
+            'A person is wise if he listens to millions of advice and doesnt implement any of it.”-Michael Bassey Johnson ',
+            'The rain that falls wherever it pleases. I want to be that rain.” -Marty Rubin',
+            'Real men dont dance to other peoples tune, instead, they play for others to dance.”-Michael Bassey Johnson',
+            'Fortune favors the nonchalant.”-Marty Rubin',
+            'He understood the code of his social class enough to affect an air of indifference about life.”-H.W. Brands',
+            'The ability to remain calm and focused in stressful situations is central to making positive decisions.”-Goldie Hawn',
+            'Be the light in the dark, be the calm in the storm and be at peace while at war.”-Michael Dolan',
+            'Let your soul stand cool and composed before a million universes.”-Walt Whitman',
+            'A calm mind helps our human intelligence to assess the situation realistically.”-Dalai Lama',
+            'Calm sailing doesn\'t come from calm waters\, it comes from having a good navigator\; a good crew and a good vessel\.-Anthony T. Hincks'
         ];
     }
 };
